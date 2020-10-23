87bca096489be0b97b88cfec458cfa81fd669cd6
Pavel Zakharoff <pavel.zakharoff@gmail.com>
2020-10-23 15:16:44 +0300

    chore(nuxt.config): add router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/learn-nuxt-content/',
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'nuxt-blog-with-content',
